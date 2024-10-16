6ba48181d95a6a09dd1b119555b9e40930eb767b
JH <201667@jnu.ac.kr>
2024-10-16 18:35:24 +0900

    chore: 기존 파일 구조 삭제

--- src/main/java/com/jpastudy/clonegram/domain/user/ProfileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jpastudy.clonegram.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-
-}
--- src/main/java/com/jpastudy/clonegram/domain/user/dto/response/UpdateUserProfileResponse.java
+++
@@ -1,9 +0,0 @@
-package com.jpastudy.clonegram.domain.user.dto.response;
-
-import lombok.Builder;
-
-@Builder
-public record UpdateUserProfileResponse(
-        String response
-) {
-}
