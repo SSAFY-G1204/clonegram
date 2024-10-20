0290f18295a084e929cb8f5eb005a7ccce279cf2
JH <201667@jnu.ac.kr>
2024-10-20 23:12:56 +0900

    refactor: UserService, UserController에 DTO 네이밍 변경 반영

--- src/main/java/com/jpastudy/clonegram/domain/UserInfoManagement/controller/UserController.java
+++ src/main/java/com/jpastudy/clonegram/domain/UserInfoManagement/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.jpastudy.clonegram.domain.UserInfoManagement.controller;
 
 import com.jpastudy.clonegram._core.utils.ApiUtils;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.GetUserProfileResponse;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileResponse;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.GetUserProfileResponseDto;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileResponseDto;
 import com.jpastudy.clonegram.domain.UserInfoManagement.service.UserService;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileRequest;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final UserService userService;
     @GetMapping("/users/{userId}")
-    public ResponseEntity<ApiUtils.ApiResult<GetUserProfileResponse>> getUserProfile(@PathVariable("userId") Long userId) {
-        GetUserProfileResponse responseDTO = userService.getUserProfile(userId);
+    public ResponseEntity<ApiUtils.ApiResult<GetUserProfileResponseDto>> getUserProfile(@PathVariable("userId") Long userId) {
+        GetUserProfileResponseDto responseDTO = userService.getUserProfile(userId);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
     @PutMapping("/users/{userId}")
-    public ResponseEntity<ApiUtils.ApiResult<UpdateUserProfileResponse>> updateUserProfile(@PathVariable("userId") Long userId, @RequestBody UpdateUserProfileRequest requestDTO){
-        UpdateUserProfileResponse responseDTO = userService.updateUserProfile(userId, requestDTO);
+    public ResponseEntity<ApiUtils.ApiResult<UpdateUserProfileResponseDto>> updateUserProfile(@PathVariable("userId") Long userId, @RequestBody UpdateUserProfileRequestDto requestDTO){
+        UpdateUserProfileResponseDto responseDTO = userService.updateUserProfile(userId, requestDTO);
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
 
--- src/main/java/com/jpastudy/clonegram/domain/UserInfoManagement/service/UserService.java
+++ src/main/java/com/jpastudy/clonegram/domain/UserInfoManagement/service/UserService.java
@@ -3,9 +3,9 @@ package com.jpastudy.clonegram.domain.UserInfoManagement.service;
 import com.jpastudy.clonegram._core.errors.exception.Exception400;
 import com.jpastudy.clonegram.domain.UserInfoManagement.dao.Profile;
 import com.jpastudy.clonegram.domain.UserInfoManagement.dao.User;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileRequest;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.GetUserProfileResponse;
-import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileResponse;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileRequestDto;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.GetUserProfileResponseDto;
+import com.jpastudy.clonegram.domain.UserInfoManagement.dto.UpdateUserProfileResponseDto;
 import com.jpastudy.clonegram.domain.UserInfoManagement.repository.ProfileRepository;
 import com.jpastudy.clonegram.domain.UserInfoManagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +21,12 @@ public class UserService {
     private final ProfileRepository profileRepository;
     private final UserRepository userRepository;
 
-    public GetUserProfileResponse getUserProfile(Long userId){
+    public GetUserProfileResponseDto getUserProfile(Long userId){
         Profile profile = profileRepository.findById(userId).orElseThrow(
                 () -> new Exception400("프로필을 찾을 수 없습니다")
         );
 
-        return GetUserProfileResponse.builder()
+        return GetUserProfileResponseDto.builder()
                 .userId(profile.getUserId())
                 .profileImg(profile.getProfileImg())
                 .profileCtt(profile.getProfileCtt())
@@ -36,14 +36,14 @@ public class UserService {
     }
 
     @Transactional
-    public UpdateUserProfileResponse updateUserProfile(Long userId, UpdateUserProfileRequest requestDTO){
+    public UpdateUserProfileResponseDto updateUserProfile(Long userId, UpdateUserProfileRequestDto requestDTO){
         Profile profile = profileRepository.findById(userId).orElseThrow(
                 () -> new Exception400("프로필을 찾을 수 없습니다")
         );
 
         profile.updateProfile(requestDTO.profileImg(), requestDTO.profileCtt());
 
-        return UpdateUserProfileResponse.builder().message("프로필 업데이트를 완료하였습니다.").build();
+        return UpdateUserProfileResponseDto.builder().message("프로필 업데이트를 완료하였습니다.").build();
     }
 
     public User findByEmail(String email) {
